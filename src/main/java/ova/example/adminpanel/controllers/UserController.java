18eb8dc5ae2be45981f0334bb139e11b83d07e0e
Vika <yan-vichka@mail.ru>
2021-02-12 21:31:47 +0300

    add method into UserController getAllUsers() and getUserById()

--- src/main/java/ova/example/adminpanel/controllers/UserController.java
+++ src/main/java/ova/example/adminpanel/controllers/UserController.java
@@ -1,7 +1,29 @@
 package ova.example.adminpanel.controllers;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ova.example.adminpanel.models.User;
+import ova.example.adminpanel.repository.UserRepository;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUser(){
+        return userRepository.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Object getUserById(@PathVariable long id){
+        return userRepository.findById(id);
+    }
 }
--- src/main/java/ova/example/adminpanel/repository/UserRepository.java
+++ src/main/java/ova/example/adminpanel/repository/UserRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ova.example.adminpanel.models.User;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository <User, Long> {
+
+    List<User> findAllUsers();
 
 }
